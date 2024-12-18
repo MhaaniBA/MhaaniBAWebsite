99e523bd7df61b96afd59383134b283d60f3b614
MhaaniBA <musaithaani@gmail.com>
2024-12-18 11:50:24 +0530

    Basic changes

---
+++ constants/constants.js
@@ -0,0 +1,64 @@
+export const NAME = 'Musaitha Haani Baheer Ahemad';
+
+export const DESIGNATION = `MERN Stack Developer | Open Source Contributor | Tech Blogger`;
+
+export const DETAILS = {
+  Residence: 'Srilanka',
+  City: 'Batticaloa',
+  // Age: '23',
+};
+
+export const SKILLS = [
+  {
+    title: 'Full Stack Developer',
+    level: '50%',
+  },
+  {
+    title: 'React Developer',
+    level: '50%',
+  },
+  // {
+  //   title: 'Desktop Application Developer',
+  //   level: '75%',
+  // },
+  {
+    title: 'Backend Developer',
+    level: '45%',
+  },
+  {
+    title: 'Technical Blogger',
+    level: '75%',
+  },
+];
+
+export const TECH_STACK = [
+  'JavaScript',
+  'ReactJS',
+  'NextJS',
+  'TypeScript',
+  'NodeJS',
+  'CSS',
+  'SCSS',
+  'TailwindCSS',
+  'Ant Design',
+  'Material UI',
+  'Next UI',
+  'REST API',
+  'MongoDB',
+  'MySQL',
+  'Git',
+  'GitHub',
+  'Figma',
+  
+];
+
+export const CONTACTS = {
+  EMAIL: 'musaithaani@gmail.com',
+  // PHONE: '763195429',
+};
+export const SOCIAL_LINKS = {
+  GITHUB: 'https://github.com/MhaaniBA',
+  LINKEDIN: 'www.linkedin.com/in/musaitha-basheer',
+  // TWITTER: 'https://twitter.com/iosamajavaid',
+  // FACEBOOK: 'https://www.facebook.com/iosamajavaid',
+};
